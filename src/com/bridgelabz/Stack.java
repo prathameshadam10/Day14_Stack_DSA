9f5b6e198d077f50d16091724c02e4459c7b4ee0
prathameshadam10 <121919043+prathameshadam10@users.noreply.github.com>
2023-03-23 22:20:14 +0530

    Update Stack.java

--- src/com/bridgelabz/Stack.java
+++ src/com/bridgelabz/Stack.java
@@ -1,4 +1,28 @@
-package com.bridgelabz;
+package com.bridgelabz.stack;
 
-public class Stack {
+import com.bridgelabz.LinkedList;
+import com.bridgelabz.Node;
+
+public class Stack<T> {
+
+
+LinkedList<T> linkedList = new LinkedList<>();
+
+    public void push(T data) {
+        linkedList.add(data);
+    }
+
+    public void show() {
+        linkedList.show();
+    }
+
+
+    public T pop() {
+        return linkedList.pop();
+    }
+    public void peek(){
+        Integer peeked = (Integer) linkedList.pop();
+        System.out.println("The Peek element is " + peeked);
+        linkedList.push((T) peeked);
+    }
 }
