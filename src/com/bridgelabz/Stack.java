a07e76819885f6ff0ce5f19888fea3d730bd3677
prathameshadam10 <121919043+prathameshadam10@users.noreply.github.com>
2023-03-23 20:29:14 +0530

    Update Stack.java

--- src/com/bridgelabz/Stack.java
+++ src/com/bridgelabz/Stack.java
@@ -1,15 +1,22 @@
 package com.bridgelabz.stack;
 
 import com.bridgelabz.LinkedList;
+import com.bridgelabz.Node;
 
 public class Stack<T> {
+
+
 LinkedList<T> linkedList = new LinkedList<>();
 
-    public void add(T data) {
+    public void push(T data) {
         linkedList.add(data);
     }
 
     public void show() {
         linkedList.show();
     }
+
+
+
+
 }
