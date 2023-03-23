234954a95ea6de6dc1061d35e4ab69d0e2cd5697
prathameshadam10 <121919043+prathameshadam10@users.noreply.github.com>
2023-03-23 20:30:15 +0530

    Update StackMain.java

--- src/com/bridgelabz/StackMain.java
+++ src/com/bridgelabz/StackMain.java
@@ -3,9 +3,10 @@ package com.bridgelabz.stack;
 public class StackMain {
     public static void main(String[] args) {
     Stack<Integer> stack = new Stack<>();
-        stack.add(70);
-        stack.add(30);
-        stack.add(56);
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
         stack.show();
+
     }
 }
